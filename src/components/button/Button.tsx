ef210a9fcf7b4692270854b9f56935f8a4d560ac
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-12-26 06:05:04 +0530

    feat: update button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,23 @@
-import { FC } from "react";
+import { FC, ButtonHTMLAttributes } from "react";
 
-type Props = {
-  text: String;
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
+}
+
+const Button: FC<ButtonProps> = ({ variant, ...props }) => {
+  const styles = () => {
+    const propStyle = props.style ?? {};
+    if (props.disabled) return propStyle;
+    if (variant === 'primary') return { borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA', ...propStyle };
+    if (variant === 'secondary') return { borderColor: '#6366F1', backgroundColor: '#FAFAFA', color: '#6366F1', ...propStyle };
+    return propStyle;
+  };
+
+  return (
+    <button {...props} style={styles()}>
+      {props.children}
+    </button>
+  );
 };
-const Button: FC<Props> = ({ text }) => {
-  return <button>{text}</button>;
-};
+
 export default Button;
