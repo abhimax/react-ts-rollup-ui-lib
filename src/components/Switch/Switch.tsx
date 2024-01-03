00740e60e6be14eff671ec32bf1af5f76f473a44
Abhiman Ranaweera <abhiman.max@gmail.com>
2024-01-03 23:12:49 +0530

    refactor(switch): cleanup code for  readability

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,28 +1,15 @@
 import React, { useState } from 'react';
 import ToggleProps from './Switch.d';
-import './switch.scss'; // Assuming you have a separate CSS file
-
-// interface ToggleProps {
-//     option1: string;
-//     option2: string;
-//     width: string;
-// }
+import './switch.scss';
 
 const Toggle: React.FC<ToggleProps> = ({ option1, option2, width }) => {
-    const [isActive, setIsActive] = useState(true); // true for option1, false for option2
-
-    const toggleOption = () => {
-        setIsActive(!isActive);
-    };
-
+    const [isActive, setIsActive] = useState(true);
     const containerStyle = {
         width,
-        // additional container styling
     };
 
     const buttonStyle = {
-        width: `calc(${width} / 2)`, // each button is half the width of the container
-        // additional button styling
+        width: `calc(${width} / 2)`,
     };
 
     return (
@@ -39,7 +26,6 @@ const Toggle: React.FC<ToggleProps> = ({ option1, option2, width }) => {
                 onClick={() => setIsActive(false)}>
                 {option2}
             </div>
-            <div className={`toggle-indicator ${isActive ? 'left' : 'right'}`} style={buttonStyle}></div>
         </div>
     );
 };
